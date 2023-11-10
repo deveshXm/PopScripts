0ab67a0dd1ea005cab85817509d735f6c418c140
Devesh Meena <nothefakedevesh@gmail.com>
2023-11-10 08:20:33 +0530

    fix: edge installation script

--- scripts/pre_install.sh
+++ scripts/pre_install.sh
@@ -1,25 +1,23 @@
-#!/bin/bash
+#!/bin/bash -e
 
 # Install essential packages
-sudo apt-get install -y curl wget git software-properties-common vim ca-certificates gnupg
+sudo apt-get install -y curl wget git software-properties-common vim ca-certificates gnupg || true
 # System Update and Upgrade
-sudo apt update
-sudo apt install --fix-missing -y
-sudo apt upgrade --allow-downgrades -y
-sudo apt full-upgrade --allow-downgrades -y
-sudo npm install -g npm@latest
+sudo apt update || true
+sudo apt install --fix-missing -y || true
+sudo apt upgrade --allow-downgrades -y || true
+sudo apt full-upgrade --allow-downgrades -y || true
+sudo npm install -g npm@latest || true
 
 # Flatpak Update
-flatpak update -y
+flatpak update -y || true
 
 # Flatpak Clean Up
-flatpak uninstall --delete-data -y
-flatpak uninstall --unused -y
+flatpak uninstall --delete-data -y || true
+flatpak uninstall --unused -y || true
 
 # System Clean Up
-sudo apt install -f
-sudo apt autoremove -y
-sudo apt autoclean
-sudo apt clean
-
-echo "Updated"
+sudo apt install -f || true
+sudo apt autoremove -y || true
+sudo apt autoclean || true
+sudo apt clean || true
